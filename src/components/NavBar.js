0f2724799bc08129dd2db5877c83b4098faa4aab
Yassamin <bagheryyasamin83@gmail.com>
2026-07-25 19:11:44 +0330

    first commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,12 +5,9 @@ const NavigationBar = () => {
   return (
     <div className="NavigationBar">
       <h1>My App</h1>
-
       <div className="topnav-right">
         <h3>Dashboard</h3>
-
         <h3>Login</h3>
-
         <h3>Registeration</h3>
       </div>
     </div>
